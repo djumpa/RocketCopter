654feac09335e247f144d4dd9e84a025f68672b1
djumpa <fildarak@seznam.cz>
2019-05-09 23:44:43 +0200

    Tested with larger files and actual following logic.

--- ArduCopter/mode_hoverslam.cpp
+++ ArduCopter/mode_hoverslam.cpp
@@ -2,15 +2,27 @@
 #include <string>
 #include <vector>
 
-#define CSV_BUFFER_SIZE 64
+#define CSV_BUFFER_SIZE 64384
 /*
  * Init and run calls for stabilize flight mode
  */
+
+uint vector_num = 0;
+
+std::vector<float> vector_thrust;
+std::vector<float> vector_pitch;
+std::vector<float> vector_roll;
 
 // stabilize_run - runs the main stabilize controller
 // called at 400Hz
 bool Copter::ModeHoverSlam::init(bool ignore_checks)
 {
+    vector_thrust.clear();
+    vector_pitch.clear();
+    vector_roll.clear();
+
+    vector_num = 0;
+
     const char *file_path = "hoverslam.csv";
     int fd;
     char buffer[CSV_BUFFER_SIZE];
@@ -28,9 +40,7 @@ bool Copter::ModeHoverSlam::init(bool ignore_checks)
     char num[CSV_BUFFER_SIZE];
     ssize_t line_index = 0;
 
-    std::vector<float> vector_thrust;
-    std::vector<float> vector_pitch;
-    std::vector<float> vector_roll;
+    ssize_t total_elements = 0;
 
     for (ssize_t i = 0; i < ret; i++)
     {
@@ -45,6 +55,7 @@ bool Copter::ModeHoverSlam::init(bool ignore_checks)
 
             ssize_t num_index = 0;
             ssize_t col_num = 0;
+
             for (ssize_t j = 0; j < line_size; j++)
             {
                 if (line[j] == ',' || j == line_size - 1)
@@ -56,7 +67,7 @@ bool Copter::ModeHoverSlam::init(bool ignore_checks)
                         //in case of last number, we need to add extra char
                         num_size = j - num_index + 1;
                     }
-                    col_num++;
+                    
 
                     memset(num, 0, sizeof(num));
                     memcpy(num, line + num_index, num_size);
@@ -64,39 +75,58 @@ bool Copter::ModeHoverSlam::init(bool ignore_checks)
 
                     switch (col_num)
                     {
-                    case 0:
+                    case 0:   
                         vector_thrust.push_back(atof(num));
                         break;
                     case 1:
-                        vector_pitch.push_back(atof(num));
+                        total_elements++;
+                        vector_pitch.push_back(atof(num)*100.0f);
                         break;
-                    case 2:
+                    case 2:   
                         vector_roll.push_back(atof(num));
                         break;
-
                     default:
                         break;
                     }
-                    
 
-                    //hal.console->printf("Num: %.2f", atof(num));
+                    col_num++;
+
+                    //hal.console->printf("Thrust: %.2f", atof(num));
                 }
 
             }
         }
     }
 
+    //hal.console->printf("Vector size: %ld", total_elements);
+    hal.console->printf("vector_thrust: %lu\n", vector_thrust.size());
+    hal.console->printf("vector_pitch: %lu\n", vector_pitch.size());
+    hal.console->printf("vector_roll: %lu\n", vector_roll.size());
+
+
+
     return true;
 }
 
 void Copter::ModeHoverSlam::run()
 {
-
+    if (vector_num<(uint)vector_thrust.size()) {
     // call attitude controller
-    attitude_control->input_euler_angle_roll_pitch_yaw(1500.0f, 0.0f, 0.0f, false);
+    attitude_control->input_euler_angle_roll_pitch_yaw(0.0f,vector_pitch[vector_num], 0.0f, false);
 
     // output throttle
-    attitude_control->set_throttle_out(0.5f,
+    attitude_control->set_throttle_out(vector_thrust[vector_num],
                                        true,
                                        g.throttle_filt);
+    
+    vector_num++;
+    } else {
+           // call attitude controller
+    attitude_control->input_euler_angle_roll_pitch_yaw(0.0f, 0.0f, 0.0f, false);
+
+    // output throttle
+    attitude_control->set_throttle_out(0.0f,
+                                       true,
+                                       g.throttle_filt); 
+    }
 }
